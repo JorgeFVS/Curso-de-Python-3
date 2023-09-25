9b5a65698ee5be7c177ff9c7ccae89fda031a60a
JorgeFVS <jorgevieira99@hotmail.com>
2023-09-25 10:22:21 -0300

    Feat: O ponto de vista do __main__ pode te confundir em módulos e pacotes Python

--- aula99_package/modulo.py
+++ aula99_package/modulo.py
@@ -4,6 +4,8 @@
 #     'nova_variavel',
 # ]
 
+from aula99_package.modulo_b import fala_oi
+
 variavel = 'Alguma coisa'
 
 
@@ -12,3 +14,4 @@ def soma_do_modulo(x, y):
 
 
 nova_variavel = 'OK'
+fala_oi()
